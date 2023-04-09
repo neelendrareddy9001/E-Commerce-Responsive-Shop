45a6fb107438340e6b7bd26ff75487af4c928613
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-04-09 23:25:57 +0200

    Add files via upload

---
+++ src/components/Home/Banner/Banner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import BannerImg from '../../../assets/banner-img.png';
+
+const Banner = () => {
+  return (
+    <div>
+        Banner
+    </div>
+  )
+}
+
+export default Banner
