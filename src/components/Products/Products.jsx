60394afe7280ef0645e52983744d47cac93402a6
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-04-10 22:03:48 +0200

    Add files via upload

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Products.scss';
+import Product from './Product/Product';
+
+const Products = () => {
+  return (
+    <div className='products-container'>
+        <div className='sec-heading'>Section Heading</div>
+        <div className='products'>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+            <Product/>
+        </div>
+    </div>
+  )
+}
+
+export default Products
