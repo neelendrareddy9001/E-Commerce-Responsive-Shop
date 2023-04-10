f9201e84b47ed2a706d7ef354b98584f9e0b757e
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-04-10 21:42:01 +0200

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 import './Home.scss';
+import Banner from './Banner/Banner';
+import Category from '../Category/Category';
+
 
 const Home = () => {
   return (
-    <div className='home'>Home</div>
+    <div>
+      <Banner/>
+      <div className='main-content'>
+        <div className='layout'>
+          <Category/>
+        </div>
+      </div>
+    </div>
   )
 }
 
