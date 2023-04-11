8751f3c04ef02e697a7f500d089ff4e4195232e4
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-04-11 19:15:44 +0200

    Update SingleProduct.jsx

--- src/components/SingleProduct/SingleProduct.jsx
+++ src/components/SingleProduct/SingleProduct.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React from 'react';
+import './SingleProduct.scss';
+import RelatedProduct from './RelatedProduct/RelatedProduct';
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram,
+  FaLinkedinIn,
+  FaPinterest,
+  FaCartPlus
+} from 'react-icons/fa';
+import prod from '../../assets/Products-Images/earbuds-prod-1.webp';
 
 const SingleProduct = () => {
   return (
-    <div>SingleProduct</div>
-  )
-}
+    <div className='single-product-main-content'>
+        <div className='layout'>
+          <div className='single-product-page'>
+            <div className='left'>
+              <img src={prod} alt="" />
+            </div>
+            <div className='right'>
+              <span className='name'>Product name</span>
+              <span className='price'>Price</span>
+              <span className='desc'>Producut description</span>
+
+              <div className='cart-buttons'>
+                <div className='quantity-buttons'>
+                  <span>-</span>
+                  <span>5</span>
+                  <span>+</span>
+                </div>
+                <button className='add-to-cart-button'>
+                  <FaCartPlus size={20} />
+                  ADD TO CART
+                </button>
+              </div>
+              <div className='divider'></div>
+              <div className='info-item'>
+                <span className='text-info'>
+                  Category:
+                  <span>Headphones</span>
+                </span>
+                <span className='text-bold'>
+                  Share:
+                  <span className='social-iconsn'>
+                    <FaFacebookF size={16} />
+                    <FaTwitter size={16} />
+                    <FaInstagram size={16} />
+                    <FaLinkedinIn size={16} />
+                    <FaPinterest size={16} />
+                  </span>
+                </span>
+              </div>
+            </div>
+          </div>
+          <RelatedProduct/>
+        </div>
+    </div>
+  );
+};
 
 export default SingleProduct
