472ab043cad34fd9462b9f50ef8e9c9c6d6e27a9
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-04-11 20:19:22 +0200

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './Home.scss';
 import Banner from './Banner/Banner';
 import Category from '../Category/Category';
+import Products from '../Products/Products';
 
 
 const Home = () => {
@@ -11,6 +12,7 @@ const Home = () => {
       <div className='main-content'>
         <div className='layout'>
           <Category/>
+          <Products headingText="Popular Products"/>
         </div>
       </div>
     </div>
