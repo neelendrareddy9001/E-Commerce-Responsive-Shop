188379544336ea2b90719e38149ce470bd40f374
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-04-11 20:37:01 +0200

    Update Category.jsx

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,27 +1,14 @@
 import React from 'react';
+import Products from '../Products/Products';
 import './Category.scss';
-import cart1 from '../../assets/category/cat-1.jpg';
-import cart2 from '../../assets/category/cat-2.jpg';
-import cart3 from '../../assets/category/cat-3.jpg';
-import cart4 from '../../assets/category/cat-4.jpg';
 
 const Category = () => {
   return (
-    <div className='shop-by-category'>
-      <div className='categories'>
-        <div className='category'>
-          <img src={cart1} alt="" />
+    <div className='category-main-content'>
+        <div className='layout'>
+          <div className='category-title'>Category Title</div>
+          <Products innerPage={true}/>
         </div>
-        <div className='category'>
-          <img src={cart2} alt="" />
-        </div>
-        <div className='category'>
-          <img src={cart3} alt="" />
-        </div>
-        <div className='category'>
-          <img src={cart4} alt="" />
-        </div>
-      </div>
     </div>
   )
 }
