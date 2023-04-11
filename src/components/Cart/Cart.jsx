74cd135149730ebc40e203bc60ee77e819812db7
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-04-11 22:22:54 +0200

    Add files via upload

---
+++ src/components/Cart/Cart.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Cart.scss';
+
+const Cart = () => {
+  return (
+    <div className='cart-panel'>
+        <div className='opac-layer'></div>
+        <div className='cart-content'>sdfklsjl</div>
+    </div>
+  )
+}
+
+export default Cart
