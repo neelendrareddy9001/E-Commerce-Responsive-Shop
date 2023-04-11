c8b93b9f3e6dad273bb38b93ef1885e0a4abdfcd
Neelendrareddy <31968527+neelendrareddy9001@users.noreply.github.com>
2023-04-11 22:26:20 +0200

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,8 +6,11 @@ import {TbSearch} from 'react-icons/tb';
 import {CgShoppingCart} from 'react-icons/cg';
 import {AiOutlineHeart} from 'react-icons/ai';
 
+import Cart from '../Cart/Cart';
+
 const Header = () => {
   const [scrolled, setScrolled] = useState(false);
+  const [showCart, setShowCart] = useState(false);
   const handleScroll = () => {
     const offset = window.scrollY;
     if(offset > 200) {
@@ -21,6 +24,7 @@ const Header = () => {
     window.addEventListener("scroll", handleScroll);
   },[]);
   return (
+    <>
     <header className={`main-header ${scrolled ? "sticky-header" : ""}`}>
       <div className='header-content'>
         <ul className='left'>
@@ -32,13 +36,15 @@ const Header = () => {
         <div className='right'>
           <TbSearch/>
           <AiOutlineHeart/>
-          <span className='cart-icon'>
+          <span className='cart-icon' onClick={() => setShowCart(true)}>
             <CgShoppingCart/>
             <span>5</span>
           </span>
         </div>
       </div>
     </header>
+    {showCart && <Cart setShowCart={setShwoCart}/>}
+    </>
   )
 }
 
