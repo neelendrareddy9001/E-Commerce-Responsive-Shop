8c12b8dbdb322ae41bd0970c61f0211c6357991a
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-09-01 23:29:47 +0200

    Searchbar style and functionality updated

--- src/components/Header/Search/Search.jsx
+++ src/components/Header/Search/Search.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 import "./Search.scss";
+import { MdClose } from "react-icons/md";
 
-const Search = () => {
-  return <div>Search</div>;
+const Search = ({ setShowSearch }) => {
+  return (
+    <div className="search-model">
+      <div className="form-field">
+        <input type="text" autoFocus placeholder="Search for products" />
+        <MdClose onClick={() => setShowSearch(false)} />
+      </div>
+    </div>
+  );
 };
 
 export default Search;
