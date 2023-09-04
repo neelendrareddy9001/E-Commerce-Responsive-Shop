33a6805df62472978d46b733a195fb7227cd0b2e
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-09-04 22:56:17 +0200

    changes had been updated

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Home from './components/Home/Home';
-import Category from './components/Category/Category';
-import SingleProduct from './components/SingleProduct/SingleProduct';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import Newsletter from './components/Footer/Newsletter/Newsletter';
-import AppContext from './utils/content';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.scss";
+import Home from "./components/Home/Home";
+import Category from "./components/Category/Category";
+import SingleProduct from "./components/SingleProduct/SingleProduct";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Newsletter from "./components/Footer/Newsletter/Newsletter";
+import AppContext from "./utils/content";
 
 function App() {
   return (
     <BrowserRouter>
       {/* This component is called higher order component */}
       <AppContext>
-        <Header/>
+        <Header />
         <Routes>
-            <Route path='/' element={<Home/>}/>
-            <Route path='/category/:id' element={<Category/>}/>
-            <Route path='/product/:id' element={<SingleProduct/>}/>
-        </Routes>  
-        <Newsletter/>
-        <Footer/>
+          <Route path="/" element={<Home />} />
+          <Route path="/category/:id" element={<Category />} />
+          <Route path="/product/:id" element={<SingleProduct />} />
+        </Routes>
+        <Newsletter />
+        <Footer />
       </AppContext>
     </BrowserRouter>
   );
---
+++ src/utils/context.js
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+import { createContext } from "react";
+import { useLocation } from "react-router-dom";
+
+export const Context = createContext();
+
+const AppContext = ({ children }) => {
+  return <Context.Provider>{children}</Context.Provider>;
+};
+
+export default AppContext;
