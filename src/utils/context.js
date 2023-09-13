f96d0cee9502e5f8b5aca50ff00d30e400953774
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-09-13 21:18:26 +0200

    updated

--- src/utils/context.js
+++ src/utils/context.js
@@ -1,6 +1,6 @@
 // import { useEffect, useState } from "react";
 import { createContext } from "react";
-import { useLocation } from "react-router-dom";
+// import { useLocation } from "react-router-dom";
 
 export const Context = createContext();
 
