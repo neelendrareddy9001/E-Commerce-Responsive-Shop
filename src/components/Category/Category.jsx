b919606513998492622fe53e6ec8202bc20436cf
neelendrareddy9001 <neelendrareddy123@gmail.com>
2023-09-13 22:16:28 +0200

    category and product updated

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import Products from '../Products/Products';
-import './Category.scss';
+import React from "react";
+import Products from "../Products/Products";
+import "./Category.scss";
 
 const Category = () => {
   return (
-    <div className='category-main-content'>
-        <div className='layout'>
-          <div className='category-title'>Category Title</div>
-          <Products innerPage={true}/>
-        </div>
+    <div className="category-main-content">
+      <div className="layout">
+        <div className="category-title">Category Title</div>
+        <Products innerPage={true} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Category
+export default Category;
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import './Products.scss';
-import Product from './Product/Product';
+import React from "react";
+import "./Products.scss";
+import Product from "./Product/Product";
 
-const Products = () => {
+const Products = ({ innerPage }) => {
   return (
-    <div className='products-container'>
-        <div className='sec-heading'>Section Heading</div>
-        <div className='products'>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
-        </div>
-    </div>
-  )
-}
+    <div className="products-container">
+      {!innerPage && <div className="sec-heading">Section Heading</div>}
 
-export default Products
+      <div className="products">
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+        <Product />
+      </div>
+    </div>
+  );
+};
+
+export default Products;
